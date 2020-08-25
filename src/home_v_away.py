adfdf5dbe91db09e6bb1f8ab6bcb6d9725af1fe3
zacharyksb <53274146+zacharyksb@users.noreply.github.com>
2020-08-25 16:26:01 -0500

    add feature eng nb

---
+++ src/home_v_away.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+def home_away_df_build(team):
+    '''
+    Creates data frame with engineered features based on team passed in.
+
+
+    Parameters
+    ----------
+    team: (string)
+        Team name to build df.
+
+
+    Returns
+    ----------
+    away_df: (DataFrame)
+        DataFrame of when provided team is away team.
+    
+    home_df: (DataFrame)
+        DataFrame of when provided team is home team.
+    '''
+    pass
+    
+
+
+_
+
+
+ if __name__ == '__main__':
+     #menu for team
