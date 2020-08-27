f30f786886187ff267cafcb049335ef90e85129c
zacharyksb <53274146+zacharyksb@users.noreply.github.com>
2020-08-27 13:04:35 -0500

    scripted df build, eda

--- src/df_build.py
+++ src/df_build.py
@@ -1,5 +1,5 @@
 import numpy as np
-import pandas as import pd
+import pandas as pd
 
 def csv_to_df():
     '''
@@ -26,6 +26,7 @@ def csv_to_df():
     
     season_num = 'Season'
 
+    # add season tracker
     for i, s in enumerate(seasons[::-1]):
         s[season_num] = i + 1
 
@@ -35,18 +36,19 @@ def csv_to_df():
                 'HTAG', 'HTR', 'Referee', 'HS', 'AS', 'HST', 'AST', 'HF', 'AF', 'HC', 
                 'AC', 'HY', 'AY', 'HR', 'AR', 'Season']
 
-    described_features = ['HomeTeam', 'AwayTeam', 'Final_Goals_H', 'Final_Goals_A',
+    described_features = ['Date', 'HomeTeam', 'AwayTeam', 'Final_Goals_H', 'Final_Goals_A',
                 'Final_Result', 'Half_Goals_H', 'Half_Goals_A', 'Half_Result', 'Referee',
                 'Shots_H', 'Shots_A', 'Target_Shots_H', 'Target_Shots_A', 'Fouls_H',
                 'Fouls_A', 'Corners_H', 'Corners_A', 'Yellow_H', 'Yellow_A', 'Red_H',
                 'Red_A', 'Season']
     
+    # generate game_stats df, index and chrono order with Data field
     game_stats = ten_season_df[stat_features]
     game_stats.Date = pd.to_datetime(game_stats.Date)
     game_stats.set_index(game_stats['Date'], inplace=True)
     game_stats.columns = described_features
     # clean game_stats df and del ten_season_df & seasons df to release memory
-    game_stats.pop('Data')
+    game_stats.pop('Date')
     game_stats.pop('Half_Goals_H')
     game_stats.pop('Half_Goals_A')
     game_stats.pop('Half_Result')
@@ -57,9 +59,9 @@ def csv_to_df():
     return game_stats
 
 
-    def team_record_df_build(df, team):
+def df_team_focus(df, team):
     '''
-    Creates data frame with engineered features based on team passed in.
+    Creates data frame of team vs. opponent stats.
 
 
     Parameters
@@ -72,25 +74,144 @@ def csv_to_df():
 
     Returns
     ----------
-    away_df: (DataFrame)
-        DataFrame of when provided team is away team.
-    
-    home_df: (DataFrame)
-        DataFrame of when provided team is home team.
+    df_feature_engineering(team_focus): (Function)
+        Passes team_focus to df_feature_engineering. Returns resulting engineering feature DataFrame
     '''
+
     team_home = df[df.HomeTeam == team]
     team_away = df[df.AwayTeam == team]
-    games_played_train_set = len(team_home[team_home.Season <=8]) + len(team_away[team_away.Season <=8])
+    # dummyise H/A/D seperately to handle W/L
+    team_home, team_away = dummyize_match_results(team_home, team_away)
+    team_focus = pd.concat([team_home, team_away])
+    team_focus.Home_Final_D.fillna(0, inplace=True)
+    team_focus.Away_Final_D.fillna(0, inplace=True)
+    team_focus.Home_Final_H.fillna(0, inplace=True)
+    team_focus.Away_Final_A.fillna(0, inplace=True)
+    # add draw, away+home wins cols together; convert to int
+    team_focus['draw'] = team_focus.Home_Final_D + team_focus.Away_Final_D
+    team_focus['win'] = team_focus.Home_Final_H + team_focus.Away_Final_A
+    team_focus.draw = team_focus.draw.astype(dtype='int64')
+    team_focus.win = team_focus.win.astype(dtype='int64')
 
-    print('\n{0} W/L/D Distribution Season \'09-\'17'.format(team))
-    total_wins = np.sum(team_home[team_home.Season <=8].Final_Result == 'H') + np.sum(team_away[team_away.Season <=8].Final_Result == 'A')
-    total_loss = np.sum(team_home[team_home.Season <=8].Final_Result == 'A') + np.sum(team_away[team_away.Season <=8].Final_Result == 'H')
-    total_draw = np.sum(team_home[team_home.Season <=8].Final_Result == 'D') + np.sum(team_away[team_away.Season <=8].Final_Result == 'D')
-    print('''
-    Team Win Percentage: {0:0.2f}%
-    Team Loss Percentage: {1:0.2f}%
-    Team Draw Percentage: {2:0.2f}%'''.format((total_wins/games_played_train_set)*100, 
-                                            (total_loss/games_played_train_set)*100,
-                                            (total_draw/games_played_train_set)*100))
+    team_focus['Home_Field_Advantage'] = np.where(team_focus.HomeTeam == team, 1, 0)
+    team_focus.Home_Field_Advantage = team_focus.Home_Field_Advantage.astype(dtype='bool')
+    team_focus['opponent'] = np.where(team_focus.Home_Field_Advantage, team_focus.AwayTeam, team_focus.HomeTeam)
+
+
+    # del 
+    team_focus.pop('Final_Result')
+    team_focus.pop('Home_Final_H')
+    team_focus.pop('Away_Final_A')
+    team_focus.pop('Home_Final_D')
+    team_focus.pop('Away_Final_D')
+    del team_home
+    del team_away
     
-    return team_home, team_away
+    return df_feature_organization(team_focus)
+
+
+def dummyize_match_results(home, away):
+    '''
+    Dummy-izes Final Result for home and away seperately.
+
+
+    Parameters
+    ----------
+    away: (DataFrame)
+        DataFrame of when provided team is away team.
+    
+    home: (DataFrame)
+        DataFrame of when provided team is home team.
+
+
+    Returns
+    ----------
+    away: (DataFrame)
+        DataFrame of when provided team is away team.
+    
+    home: (DataFrame)
+        DataFrame of when provided team is home team.
+    '''
+    home_final_dummies = pd.get_dummies(home.Final_Result, prefix='Home_Final')
+    away_final_dummies = pd.get_dummies(away.Final_Result, prefix='Away_Final')
+    home = pd.concat([home, home_final_dummies], axis=1)
+    away = pd.concat([away, away_final_dummies], axis=1)
+
+    # del
+    home.pop('Home_Final_A')
+    away.pop('Away_Final_H')
+
+    return home, away
+
+
+def df_feature_organization(df):
+    '''
+    Creates data frame of team vs. opponent stats.
+
+
+    Parameters
+    ----------
+    df: (DataFrame)
+        DataFrame focused on team in question.
+
+
+    Returns
+    ----------
+    df: (DataFrame)
+        DataFrame of when provided team is away team.
+    '''
+    col_list = ['season_num', 'draw', 'win', 'home_advan', 'opponent', 'team_goals', 'oppon_goals',
+            'team_shots', 'oppon_shots', 'team_target_shots', 'oppon_target_shots', 'team_fouls', 'oppon_fouls',
+            'team_corners', 'oppon_corners', 'team_yellows', 'oppon_yellows', 'team_reds', 'oppon_reds']
+    
+    org_col_list = ['home_advan', 'team_shots', 'team_target_shots', 'team_goals', 'team_fouls', 'team_corners', 
+            'team_yellows', 'team_reds', 'opponent', 'oppon_shots', 'oppon_target_shots', 'oppon_goals', 
+            'oppon_fouls', 'oppon_corners', 'oppon_yellows', 'oppon_reds', 'season_num']
+
+    # goals
+    df['team_goals'] = np.where(df.Home_Field_Advantage, df.Final_Goals_H, df.Final_Goals_A)
+    df['oppon_goals'] = np.where(~df.Home_Field_Advantage, df.Final_Goals_H, df.Final_Goals_A)
+    #shots
+    df['team_shots'] = np.where(df.Home_Field_Advantage, df.Shots_H, df.Shots_A)
+    df['oppon_shots'] = np.where(~df.Home_Field_Advantage, df.Shots_H, df.Shots_A)
+    # target shots
+    df['team_target_shots'] = np.where(df.Home_Field_Advantage, df.Target_Shots_H, df.Target_Shots_A)
+    df['oppon_target_shots'] = np.where(~df.Home_Field_Advantage, df.Target_Shots_H, df.Target_Shots_A)
+    # fouls
+    df['team_fouls'] = np.where(df.Home_Field_Advantage, df.Fouls_H, df.Fouls_A)
+    df['oppon_fouls'] = np.where(~df.Home_Field_Advantage, df.Fouls_H, df.Fouls_A)
+    # corners
+    df['team_corners'] = np.where(df.Home_Field_Advantage, df.Corners_H, df.Corners_A)
+    df['oppon_corners'] = np.where(~df.Home_Field_Advantage, df.Corners_H, df.Corners_A)
+    # yellows
+    df['team_yellows'] = np.where(df.Home_Field_Advantage, df.Yellow_H, df.Yellow_A)
+    df['oppon_yellows'] = np.where(~df.Home_Field_Advantage, df.Yellow_H, df.Yellow_A)
+    # reds
+    df['team_reds'] = np.where(df.Home_Field_Advantage, df.Red_H, df.Red_A)
+    df['oppon_reds'] = np.where(~df.Home_Field_Advantage, df.Red_H, df.Red_A)
+
+
+    # del
+    df.pop('HomeTeam')
+    df.pop('AwayTeam')
+    df.pop('Final_Goals_H')
+    df.pop('Final_Goals_A')
+    df.pop('Shots_H')
+    df.pop('Shots_A')
+    df.pop('Target_Shots_H')
+    df.pop('Target_Shots_A')
+    df.pop('Fouls_H')
+    df.pop('Fouls_A')
+    df.pop('Corners_H')
+    df.pop('Corners_A')
+    df.pop('Yellow_H')
+    df.pop('Yellow_A')
+    df.pop('Red_H')
+    df.pop('Red_A')
+
+    df.columns = col_list
+    #sort datetime index
+    df = df.sort_index()[::-1]
+
+
+    return df[org_col_list] 
---
+++ src/epl_analysis.py
@@ -0,0 +1,61 @@
+import df_build as build
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, precision_score, recall_score
+import os
+
+
+def eda(df, team):
+    #  keep as bool??
+    # df.home_field_advantage = dfd.home_Field_Advantage.astype(dtype='int64')
+    pass
+    
+def rf_model(df):
+    # sklearn.model_selection.TimeSeriesSplit??
+    train, test = df[df.season_num <=8], df[df.season_num > 8]
+    # pop season_num, drop in assignement to keep for eda tracking?
+    y_train, y_test = train.pop('home_reds'), test.pop('home_reds')
+    X_train, X_test = train.values, test.values
+    
+    rf = RandomForestClassifier()
+    rf.fit(X_train, y_train)
+    print('Score: {0:0.2f}'.format(rf.score(X_test, y_test)*100))
+    y_predict = rf.predict(X_test)
+    print('Confusion Matrix:')
+    print(confusion_matrix(y_test, y_predict))
+
+
+
+
+
+
+if __name__ == '__main__':
+    season_20_21_team_options = ['Arsenal', 'Aston Villa', 'Brighton', 'Burnley',
+    'Chelsea', 'Crystal Palace', 'Everton', 'Fulham', 'Leicester', 'Liverpool',
+    'Man City', 'Man United', 'Newcastle', 'Southhampton', 'Tottenham', 'West Brom',
+    'West Ham', 'Wolves']
+
+    df = build.csv_to_df()
+    team_selection_prompt='\n\nSelect a team from the options below:'
+    team_selected = False
+    
+    while not team_selected:
+        print(team_selection_prompt)
+        print(season_20_21_team_options)
+        # MAKE team GLOBAL VARIABLE
+        team = input('>>>')
+
+        if team in season_20_21_team_options:
+            record = build.df_team_focus(df, team)
+            record.info()
+
+            another = input('\nWould you like to view another team? (y/n)')
+
+            if another == 'y':
+                continue
+            else:
+                print('Goodbye!')
+                team_selected = True
+        else:
+            print('\nTry Again\n')
+            continue
